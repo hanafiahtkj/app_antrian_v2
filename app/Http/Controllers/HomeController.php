e3fa9e267f34cc18d73712ea6b9825be338a077f
Hanafi_dev <hanafiahtkj@gmail.com>
2024-06-09 09:04:16 +0800

    #

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,13 +20,13 @@ class HomeController extends Controller
         $today = now()->toDateString();
         $todayAt3PM = now()->setHour(15)->setMinute(0)->setSecond(0)->toDateTimeString();
 
-        Antrian::where(function ($query) use ($today, $todayAt3PM) {
-            $query->whereDate('created_at', '<', $today)
-                ->orWhere(function ($query) use ($today, $todayAt3PM) {
-                    $query->whereDate('created_at', '=', $today)
-                            ->whereTime('created_at', '>', '23:00');
-                });
-        })->delete();
+        // Antrian::where(function ($query) use ($today, $todayAt3PM) {
+        //     $query->whereDate('created_at', '<', $today)
+        //         ->orWhere(function ($query) use ($today, $todayAt3PM) {
+        //             $query->whereDate('created_at', '=', $today)
+        //                     ->whereTime('created_at', '>', '23:00');
+        //         });
+        // })->delete();
 
         return view('welcome');
     }
